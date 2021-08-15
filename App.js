3ecbbbc264edcfd69e1b0497a2f02a1de529ddbb
suvarna-13 <76952365+suvarna-13@users.noreply.github.com>
2021-08-15 08:09:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
-
+import Home from "./Home";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Checkout from "./Checkout";
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header />
-      {/* Home */} 
+      <Switch>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+     
     </div>
+    </Router>
+
   );
 }
 
